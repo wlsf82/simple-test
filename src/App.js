03afcf0e56007f2e62d9ce2d345af67e5217647f
Walmyr Filho <wlsf82@gmail.com>
2017-08-15 06:18:00 +0200

    create basic form and its style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,61 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      <form className="Form">
+          <div>
+              <label>Description</label>
+              <input />
+              <label>Action</label>
+              <select>
+                  <option>Go to URL</option>
+              </select>
+              <label>CSS Selector</label>
+              <input />
+          </div>
+          <div>
+              <label>Description</label>
+              <input />
+              <label>Action</label>
+              <select>
+                  <option>Write text</option>
+              </select>
+              <label>CSS Selector</label>
+              <input />
+              <label>Text</label>
+              <input />
+          </div>
+          <div>
+              <label>Description</label>
+              <input />
+              <label>Action</label>
+              <select>
+                  <option>Press key</option>
+              </select>
+              <label>Key</label>
+              <select>
+                  <option>ENTER</option>
+              </select>
+              <label>CSS Selector</label>
+              <input />
+          </div>
+          <div>
+              <label>Description</label>
+              <input />
+              <label>Action</label>
+              <select>
+                  <option>Add expectation</option>
+              </select>
+              <label>Expectation</label>
+              <select>
+                  <option>Contains</option>
+              </select>
+              <label>CSS Selector</label>
+              <input />
+              <label>Text</label>
+              <input />
+          </div>
+          <button type="submit">Create</button>
+      </form>
       </div>
     );
   }
