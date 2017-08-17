5f54b4f9a4dd3898651ab3b12ee14e941525fd96
Walmyr Filho <wlsf82@gmail.com>
2017-08-18 00:50:54 +0200

    update protractor config with test report

--- server/protractor.conf.js
+++ server/protractor.conf.js
@@ -1,3 +1,5 @@
+const Jasmine2HtmlReporter = require("protractor-jasmine2-html-reporter");
+
 module.exports.config = {
     directConnect: true,
 
@@ -9,5 +11,14 @@ module.exports.config = {
 
     onPrepare() {
         browser.ignoreSynchronization = true;
+
+        jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
+            savePath: "server/test-report",
+            fileName: "e2e-test-report",
+            fixedScreenshotName: true,
+            cleanDestination: false,
+            consolidate: false,
+            takeScreenshotsOnlyOnFailures: true,
+        }));
     }
 };
