ca3031623b0c00da2d03b9a6c4441f161ba383e7
Walmyr Filho <wlsf82@gmail.com>
2017-08-19 03:38:11 +0200

    update server implementation with test suite creator helper

--- server/specs/test-suite-1.spec.js
+++ server/specs/test-suite-1.spec.js
@@ -1,3 +1,6 @@
+Helper = require("../helper");
+helper = new Helper();
+
 const sampleTestSuite =
     {
         description: "Google",
@@ -84,105 +87,19 @@ const isBeforeEachSet = true;
 const isAfterEachSet = true;
 const isAfterAllSet = true;
 
-function addBeforeAllCallback(isSet) {
-    if (isSet) {
-        beforeAll(() => {
-            mapToSteps(sampleTestSuite.beforeAll);
-        });
-    }
-}
-
-function addBeforeEachCallback(isSet) {
-    if (isSet) {
-        beforeEach(() => {
-            mapToSteps(sampleTestSuite.beforeEach);
-        });
-    }
-}
-
-function addAfterAllCallback(isSet) {
-    if (isSet) {
-        afterAll(() => {
-            mapToSteps(sampleTestSuite.afterAll);
-        });
-    }
-}
-
-function addAfterEachCallback(isSet) {
-    if (isSet) {
-        afterEach(() => {
-            mapToSteps(sampleTestSuite.afterEach);
-        });
-    }
-}
-
-function addTestCasesCallbacksOnTestSuite(testSuite) {
-    testSuite.testCases.forEach((testCase) => {
-        addStepsOnTestCaseBeasedOnDescription(testCase.description, testCase.steps);
-    });
-}
-
-function addStepsOnTestCaseBeasedOnDescription(description, steps) {
-    it(description, () => {
-        mapToSteps(steps);
-    });
-}
-
-function mapToSteps(testCase) {
-    testCase.forEach((step) => {
-        switch (step.action) {
-            case "Go to URL":
-                goToUrl(step.url);
-                break;
-            case "Enter text":
-                enterText(step.css_selector, step.text);
-                break;
-            case "Press key":
-                pressKey(step.css_selector, step.key);
-                break;
-            case "Click":
-                click(step.css_selector);
-                break;
-            case "Expect to contain":
-                expectToContain(step.css_selector, step.text);
-                break;
-        }
-    });
-}
-
-function goToUrl(url) {
-    browser.get(url);
-}
-
-function enterText(cssSelector, text) {
-    element.all(by.css(cssSelector)).first().sendKeys(text);
-}
-
-function click(cssSelector) {
-    element.all(by.css(cssSelector)).first().click();
-}
-
-function pressKey(cssSelector, key) {
-    element.all(by.css(cssSelector)).first().sendKeys(protractor.Key[key]);
-}
-
-function expectToContain(cssSelector, text) {
-    expect(element.all(by.css(cssSelector)).first().getText()).toContain(text);
-}
-
 describe(sampleTestSuite.description, () => {
     if (typeof(sampleTestSuite.testCases) === "undefined" || typeof(sampleTestSuite.testCases[0]) === "undefined") {
         console.log("Test steps are mandatory!");
         return;
     }
 
-    addBeforeAllCallback(isBeforeAllSet);
+    helper.addBeforeAllCallbackOnTestSuite(sampleTestSuite, isBeforeAllSet);
 
-    addBeforeEachCallback(isBeforeEachSet);
+    helper.addBeforeEachCallbackOnTestSuite(sampleTestSuite, isBeforeEachSet);
 
-    addTestCasesCallbacksOnTestSuite(sampleTestSuite);
+    helper.addTestCasesCallbacksOnTestSuite(sampleTestSuite);
 
-    addAfterEachCallback(isAfterEachSet);
+    helper.addAfterEachCallbackOnTestSuite(sampleTestSuite, isAfterEachSet);
 
-    addAfterAllCallback(isAfterAllSet);
+    helper.addAfterAllCallbackOnTestSuite(sampleTestSuite, isAfterAllSet);
 });
