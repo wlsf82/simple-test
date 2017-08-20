9e2b01fafff889520c7096e8c3853b4385422c8d
Walmyr Filho <wlsf82@gmail.com>
2017-08-20 23:14:15 +0200

    update steps factory and map to steps with new steps

--- server/MapToStepsHelper.js
+++ server/MapToStepsHelper.js
@@ -11,14 +11,44 @@ class MapToStepsHelper {
                 case "Enter text":
                     stepsFactory.enterText(step.css_selector, step.text);
                     break;
+                case "Clear text field":
+                    stepsFactory.clearTextField(step.css_selector);
+                    break;
                 case "Press key":
                     stepsFactory.pressKey(step.css_selector, step.key);
+                    break;
+                case "Refresh page":
+                    stepsFactory.refresh();
                     break;
                 case "Click":
                     stepsFactory.click(step.css_selector);
                     break;
                 case "Expect to contain":
                     stepsFactory.expectToContain(step.css_selector, step.text);
+                    break;
+                case "Expect not to contain":
+                    stepsFactory.expectNotToContain(step.css_selector, step.text);
+                    break;
+                case "Expect to equal":
+                    stepsFactory.expectToEqual(step.css_selector, step.text);
+                    break;
+                case "Expect not to equal":
+                    stepsFactory.expectNotToEqual(step.css_selector, step.text);
+                    break;
+                case "Expect count to be":
+                    stepsFactory.expectCountToBe(step.css_selector, step.number);
+                    break;
+                case "Expect to be displayed":
+                    stepsFactory.expectToBeDisplayed(step.css_selector);
+                    break;
+                case "Expect not to be displayed":
+                    stepsFactory.expectNotToBeDisplayed(step.css_selector);
+                    break;
+                case "Expect to be present":
+                    stepsFactory.expectToBePresent(step.css_selector);
+                    break;
+                case "Expect not to be present":
+                    stepsFactory.expectNotToBePresent(step.css_selector);
                     break;
             }
         });
