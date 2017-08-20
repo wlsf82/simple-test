20563ee20705104fefb3c5cb020b157a544ef322
Walmyr Filho <wlsf82@gmail.com>
2017-08-20 23:14:15 +0200

    update steps factory and map to steps with new steps

--- server/StepsFactory.js
+++ server/StepsFactory.js
@@ -7,6 +7,10 @@ class StepsFactory {
         element.all(by.css(cssSelector)).first().sendKeys(text);
     }
 
+    clearTextField(cssSelector) {
+        element.all(by.css(cssSelector)).first().clear();
+    }
+
     click(cssSelector) {
         element.all(by.css(cssSelector)).first().click();
     }
@@ -15,8 +19,44 @@ class StepsFactory {
         element.all(by.css(cssSelector)).first().sendKeys(protractor.Key[key]);
     }
 
+    refresh() {
+        browser.refresh();
+    }
+
     expectToContain(cssSelector, text) {
         expect(element.all(by.css(cssSelector)).first().getText()).toContain(text);
+    }
+
+    expectNotToContain(cssSelector, text) {
+        expect(element.all(by.css(cssSelector)).first().getText()).not.toContain(text);
+    }
+
+    expectToEqual(cssSelector, text) {
+        expect(element.all(by.css(cssSelector)).first().getText()).toEqual(text);
+    }
+
+    expectNotToEqual(cssSelector, text) {
+        expect(element.all(by.css(cssSelector)).first().getText()).not.toEqual(text);
+    }
+
+    expectCountToBe(cssSelector, number) {
+        expect(element.all(by.css(cssSelector)).count()).toBe(number);
+    }
+
+    expectToBeDisplayed(cssSelector) {
+        expect(element.all(by.css(cssSelector)).first().isDisplayed().toBe(true));
+    }
+
+    expectNotToBeDisplayed(cssSelector) {
+        expect(element.all(by.css(cssSelector)).first().isDisplayed().not.toBe(true));
+    }
+
+    expectToBePresent(cssSelector) {
+        expect(element.all(by.css(cssSelector)).first().isPresent().toBe(true));
+    }
+
+    expectNotToBePresent(cssSelector) {
+        expect(element.all(by.css(cssSelector)).first().isPresent().not.toBe(true));
     }
 }
 
