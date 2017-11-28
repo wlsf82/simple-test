6add42f132a153082f286428a62e58532a73862e
Jamy Golden <jamygolden@gmail.com>
2017-11-28 21:19:44 +0100

    Allow localhost:3000 to be whitelisted by server API

--- server/restful-api.js
+++ server/restful-api.js
@@ -54,6 +54,12 @@ const bodyParser = require("body-parser");
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const port = process.env.PORT || 5050;
 const router = express.Router();
