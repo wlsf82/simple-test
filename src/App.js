47e824ed3efd76bfe28714fcc791084879c4837d
Jamy Golden <jamygolden@gmail.com>
2017-11-28 21:20:05 +0100

    Populate HTML with DB data

--- src/App.js
+++ src/App.js
@@ -1,68 +1,91 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./App.css";
+
+function DataSelect({ obj, name }) {
+    return (
+        <select name={name}>
+            {obj.map((item => (
+                <option
+                    key={item.description}
+                    value={item.description}
+                >
+                    {item.description}
+                </option>
+            )))}
+        </select>
+    );
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <form className="Form">
-          <div>
-              <label>Description</label>
-              <input />
-              <label>Action</label>
-              <select>
-                  <option>Go to URL</option>
-              </select>
-              <label>CSS Selector</label>
-              <input />
-          </div>
-          <div>
-              <label>Description</label>
-              <input />
-              <label>Action</label>
-              <select>
-                  <option>Write text</option>
-              </select>
-              <label>CSS Selector</label>
-              <input />
-              <label>Text</label>
-              <input />
-          </div>
-          <div>
-              <label>Description</label>
-              <input />
-              <label>Action</label>
-              <select>
-                  <option>Press key</option>
-              </select>
-              <label>Key</label>
-              <select>
-                  <option>ENTER</option>
-              </select>
-              <label>CSS Selector</label>
-              <input />
-          </div>
-          <div>
-              <label>Description</label>
-              <input />
-              <label>Action</label>
-              <select>
-                  <option>Add expectation</option>
-              </select>
-              <label>Expectation</label>
-              <select>
-                  <option>Contains</option>
-              </select>
-              <label>CSS Selector</label>
-              <input />
-              <label>Text</label>
-              <input />
-          </div>
-          <button type="submit">Create</button>
-      </form>
-      </div>
-    );
-  }
+    static propTypes = {
+        store: PropTypes.object.isRequired,
+    }
+
+    render() {
+        const { actions, expectations } = this.props.store;
+        return (
+            <div className="App">
+            <form className="Form">
+                <div>
+                    <label>Description</label>
+                    <input />
+
+                    <label>Action</label>
+                    <DataSelect obj={actions} />
+
+                    <label>CSS Selector</label>
+                    <input />
+                </div>
+                <div>
+                    <label>Description</label>
+                    <input />
+
+                    <label>Action</label>
+                    <DataSelect obj={actions} />
+
+                    <label>CSS Selector</label>
+                    <input />
+
+                    <label>Text</label>
+                    <input />
+                </div>
+                <div>
+                    <label>Description</label>
+                    <input />
+
+                    <label>Action</label>
+                    <DataSelect obj={actions} />
+
+                    <label>Key</label>
+                    <select>
+                        <option>ENTER</option>
+                    </select>
+
+                    <label>CSS Selector</label>
+                    <input />
+                </div>
+                <div>
+                    <label>Description</label>
+                    <input />
+
+                    <label>Action</label>
+                    <DataSelect obj={actions} />
+
+                    <label>Expectation</label>
+                    <DataSelect obj={expectations} />
+
+                    <label>CSS Selector</label>
+                    <input />
+
+                    <label>Text</label>
+                    <input />
+                </div>
+                <button type="submit">Create</button>
+            </form>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ActionService, ExpectationService, ProjectService, StepService, TestService } from "./client-api";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const apiUri = "http://localhost:5050/api";
+const apiClient = axios;
+const actionService = new ActionService({ apiClient, apiUri, name: "actions" });
+const expectationService = new ExpectationService({ apiClient, apiUri, name: "expectations" });
+const projectService = new ProjectService({ apiClient, apiUri, name: "projects" });
+const stepService = new StepService({ apiClient, apiUri, name: "steps" });
+const testService = new TestService({ apiClient, apiUri, name: "tests" });
+
+Promise.all([
+    actionService.fetchAll(),
+    expectationService.fetchAll(),
+    projectService.fetchAll(),
+    stepService.fetchAll(),
+    testService.fetchAll(),
+]).then((data) => {
+    const store = [
+        actions,
+        expectations,
+        projects,
+        steps,
+        tests,
+    ];
+
+    ReactDOM.render(<App store={store} />, document.getElementById('root'));
+});
+
 registerServiceWorker();
